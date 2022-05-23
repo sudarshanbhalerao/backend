2437ec659cb55fbcab8defeafa3a8f18f9e19ba4
P <sivashankar.palanivel@atos.net>
2022-05-23 17:39:51 +0530

    initial commit

---
+++ src/test/java/com/petclinic/api/runner/TestRunner.java
@@ -0,0 +1,16 @@
+package com.petclinic.api.runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+		features = { "./src/test/java/com/petclinic/api" }, glue = { "com.petclinic.api.stepDefinitions" }, tags = {
+				"@Regression" }, plugin = {
+						"com.cucumber.listener.ExtentCucumberFormatter:reports//API_Report.html",
+						"html:output/html-reports" }, monochrome = true)
+public class TestRunner {
+
+}
