ed35421504ec4bac36773740bdc03c58ff30d1c2
SriDhanusiyaNavarathS <sridhanusiyanavarath2109@gmail.com>
2022-05-23 17:59:58 +0530

    Petclinic validation features

---
+++ ui-test/data/testdata.ts
@@ -0,0 +1,24 @@
+const data = {
+    APP_URL: {
+        BASE_URL: "https://spring-petclinic-community.herokuapp.com/"
+    },
+
+    PageTitle: {
+        title: "PetClinic :: a Spring Framework demonstration"
+    },
+
+    AddOwner: {
+        firstName: 'Add New Owner',
+        lastName: "A",
+        address: "112, New street, India",
+        city: "Chennai",
+        telephone: "9876543622",
+        invalidtelephone: "abcd123e"
+    },
+    FieldValidationErrorMessage: {
+        Errormsg: "must not be empty",
+        TelephonevalidationErrorMsg: "numeric value out of bounds (<10 digits>.<0 digits> expected)"
+    }
+
+}
+export default data;
